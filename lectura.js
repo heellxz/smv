143f7423aa1b0c4c872a6f7d86ca3c8f6b81023a
heellxz <heellxz@gmail.com>
2017-06-30 00:58:20 -0400

    primer commit

---
+++ lectura.js
@@ -0,0 +1,45 @@
+var timer = setInterval(refresh, 30000);
+var data = [];
+
+function refresh () {
+	var nickname = $('#nickname').val();
+	
+	steem.api.getAccounts([nickname], function(err, response){
+		console.log(response);
+		var balance = response[0]['balance'] + ' + ' + response[0]['sbd_balance'];
+		$('#balance').html(balance);
+	});
+
+	steem.api.getOrderBook(1, function(err, response){
+		var ask = response['asks'][0]['real_price'];
+		var bids = response['bids'][0]['real_price'];
+		var milli = (new Date).getTime();
+
+		data.push({'a' : Math.round(ask*100000)/100000, 'b' : Math.round(bids*100000)/100000, "y" : milli});
+
+		if(data.length > 100){
+			data.shift();
+		};
+
+		grafo.setData(data);
+	});
+};
+
+var grafo = Morris.Line({
+	element: 'grafo',
+	xkey: 'y',
+	hideHover: false,
+	xlabels: "second",
+	ykeys: ['a', 'b'],
+	labels: ['ask', 'bid'],
+	lineColors: ['#FF1111', '#11FF11'],
+	smooth: false,
+	ymax: 'auto',
+	ymin: 'auto',
+	axes: false,
+	grid: false,
+	pointSize: 10
+
+});
+
+refresh();
